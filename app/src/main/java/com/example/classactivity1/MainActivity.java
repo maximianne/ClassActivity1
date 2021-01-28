4fcfaee23cd8ac1ce3a48a4f89d0811a8c449742
Maximianne Castaneda <mcastaneda2@oxy.edu>
2021-01-28 12:23:19 -0800

    First commit

---
+++ app/src/main/java/com/example/classactivity1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.classactivity1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
